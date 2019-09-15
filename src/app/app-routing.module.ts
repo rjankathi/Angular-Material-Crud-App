168a7d7ffb6fa258fa8bbef82105bcbab62fbfa4
Raj Ankathi <nankathi@gmail.com>
2019-09-15 01:59:35 -0700

    Lazy Loading ContactsModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // 1st route
+  {path: '', loadChildren: './features/contactc/contacts.module#ContactsModule'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+      routes,
+      {
+      enableTracing: true , // Debugging purpose only
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
