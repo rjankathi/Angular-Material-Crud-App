58ddf44cff400436bd2cfdd269c02ee28b64ab00
Raj Ankathi <nankathi@gmail.com>
2019-09-15 01:56:45 -0700

    Contacts Routing Module and contacts Module

--- src/app/features/contacts/contacts-routing.module.ts
+++ src/app/features/contacts/contacts-routing.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { SharedModule } from '@app/shared/shared.module';
-import { MaterialFormsModule } from '@app/shared/forms/material-forms.module';
-
-import { NewContactComponent } from './new-contact/new-contact.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
-import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
 
+const contactsRoutes: Routes = [
+    // 1st Route
+    {  path: '', component: ContactListComponent },
+    // 2nd Route
+    {  path: 'add-contact', component: NewContactComponent }
+];
 
 @NgModule({
-  imports: [
-    SharedModule,
-    MaterialFormsModule,
-    ContactsRoutingModule
-  ],
-  declarations: [
-    ContactDetailComponent,
-    ContactListComponent,
-    NewContactComponent
-  ],
-  entryComponents: [
-    ContactDetailComponent
-  ]
+    imports: [
+        RouterModule.forChild(contactsRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
+
 export class ContactsRoutingModule { }
--- src/app/features/contacts/contacts.module.ts
+++ src/app/features/contacts/contacts.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { ContactsRoutingModule } from './contacts-routing.module';
+import { SharedModule } from '@app/shared/shared.module';
+import { MaterialFormsModule } from '@app/shared/forms/material-forms.module';
+
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule,
+    SharedModule,
+    MaterialFormsModule,
     ContactsRoutingModule
+  ],
+  declarations: [
+    ContactDetailComponent,
+    ContactListComponent,
+    NewContactComponent
+  ],
+  entryComponents: [
+    ContactDetailComponent
   ]
 })
-export class ContactsModule { }
+export class ContactsRoutingModule { }
