65e7aca53375a04571ebfd0d5c0150c525ae8f1e
Raj Ankathi <nankathi@gmail.com>
2019-09-15 01:34:44 -0700

    Contact Detail Form component

---
+++ src/app/features/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Contact } from '@app/features/contact/shared/contact';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.scss']
+})
+export class ContactDetailComponent implements OnInit {
+  title = 'Edit Contact';
+  form: FormGroup;
+
+  constructor(
+    private dialogRef: MatDialogRef<ContactDetailComponent>,
+    @Inject(MAT_DIALOG_DATA) public contact: Contact) { }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.form = new FormGroup({
+      id: new FormControl(this.contact.id),
+      firstName: new FormControl(this.contact.firstName, Validators.required),
+      lastName: new FormControl(this.contact.lastName, Validators.required),
+      email: new FormControl(this.contact.email, [Validators.required, Validators.email]),
+      work: new FormControl(this.contact.work, Validators.required),
+      street: new FormControl(this.contact.street, Validators.required),
+      city: new FormControl(this.contact.city, Validators.required),
+      state: new FormControl(this.contact.state, Validators.required),
+      zip: new FormControl(this.contact.zip, Validators.required)
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+        return;
+    }
+    this.dialogRef.close(this.form.value);
+}
+
+close() {
+    this.dialogRef.close(null);
+}
+}
+
