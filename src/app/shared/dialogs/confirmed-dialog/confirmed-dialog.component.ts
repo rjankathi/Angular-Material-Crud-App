a7e3541309f71bdc7b587e794330636e4feeef57
Raj Ankathi <nankathi@gmail.com>
2019-09-15 01:00:57 -0700

    Created Shared Module with Dialogs Component

---
+++ src/app/shared/dialogs/confirmed-dialog/confirmed-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-confirmed-dialog',
+  templateUrl: './confirmed-dialog.component.html',
+  styleUrls: ['./confirmed-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfirmedDialogComponent implements OnInit {
+ title: string;
+ message: string;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit() {
+    this.title = this.data.title;
+    this.message = this.data.message;
+  }
+
+}
